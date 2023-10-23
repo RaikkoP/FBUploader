986849a9d8a9665c2d9e075dc86af7666df2e61f
RaikkoP <raikko.prants@gmail.com>
2023-10-23 14:09:05 +0300

    First Commit

---
+++ raikkop-fbuploader.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package RaikkoP-FBUploader
+ */
+/**
+Plugin Name: RaikkoP-FBUploader
+Plugin URI: https://github.com/RaikkoP/FBUploader
+Description: Facebook post uploader.
+Version: 1.0.0
+Author: Raikko
+Author URI: https://raikkop.ee
+License: GPLv2 or later
+Text Domain: raikkop-dbuploader
+ */
